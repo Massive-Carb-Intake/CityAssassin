d74efd643a3e9566b025d2e8ebb380c4810fbdb9
Drew Crispino <andrewvcrispino@gmail.com>
2023-07-09 10:55:49 -0400

    Created Characteristics Script

---
+++ Assets/Scripts/Characteristics.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Characteristics : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        List<string> hats = new List<string>();
+        hats.Add("Hat 1");
+        hats.Add("Hat 2");
+        hats.Add("Hat 3");
+
+        List<string> faces = new List<string>();
+        faces.Add("Face 1");
+        faces.Add("Face 2");
+        faces.Add("Face 3");
+
+        List<string> outfits = new List<string>();
+        outfits.Add("Outfit 1");
+        outfits.Add("Outfit 2");
+        outfits.Add("Outfit 3");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
