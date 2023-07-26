0d6f8c531b177b86837da2daf11c973829c5b657
Drew Crispino <andrewvcrispino@gmail.com>
2023-07-26 10:56:48 -0400

    Worked on Collision for Interactions

--- Assets/Scripts/InteractionPoint.cs
+++ Assets/Scripts/InteractionPoint.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class InteractionPoint : MonoBehaviour
 {
+    public bool hit = false;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +16,10 @@ public class InteractionPoint : MonoBehaviour
     void Update()
     {
         
+    }
+
+    void OnCollisionEnter(Collision col)
+    {
+        Debug.Log("Hit Detected");
     }
 }
