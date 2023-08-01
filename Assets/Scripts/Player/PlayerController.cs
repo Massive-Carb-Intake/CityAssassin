13f49c6762710bf01e33a8803fd5e323191d1ee4
Maanav Rao <58347499+maarao@users.noreply.github.com>
2023-08-01 02:36:01 -0400

    Tried ot implement movement the proper way but my way's better

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -9,12 +9,12 @@ namespace Player
         [SerializeField] private float gravityScale = 5f;
         [SerializeField] private float stoppingPoint = 10f;
         [SerializeField] private float returnToStoppingPointTime = 1;
-
+        
         private Rigidbody _rigidbody;
         private GameMode _gameMode;
         private GroundChecker _groundChecker;
         private Rigidbody _mainCameraRigidbody;
-
+        
         // Start is called before the first frame update
         void Start()
         {
@@ -24,30 +24,30 @@ namespace Player
             _groundChecker = GameObject.Find("Ground_Checker").GetComponent<GroundChecker>();
             _mainCameraRigidbody = GameObject.Find("Main_Camera").GetComponent<Rigidbody>();
         }
-
+        
         // Update is called once per frame
         void Update()
         {
             HandleJump();
         }
-
+        
         private void FixedUpdate()
         {
             MoveRight();
             MoveCameraRight();
             ApplyIncreasedGravity();
         }
-
+        
         private void MoveCameraRight()
         {
             _mainCameraRigidbody.velocity = new Vector3(_gameMode.GetCurrentWorldSpeed(), 0, 0);
         }
-
+        
         private void ApplyIncreasedGravity()
         {
             _rigidbody.AddForce(Physics.gravity * ((gravityScale - 1) * _rigidbody.mass));
         }
-
+        
         private void MoveRight()
         {
             // The entire thing has to be in clamping because clamping has become the
@@ -63,7 +63,7 @@ namespace Player
                 _rigidbody.velocity = velocity;
             }
         }
-
+        
         private void HandleJump()
         {
             // Used https://gamedevbeginner.com/how-to-jump-in-unity-with-or-without-physics/ for jumping mechanic
@@ -72,7 +72,7 @@ namespace Player
                 Jump();
             }
         }
-
+        
         public void Jump()
         {
             if (CanJump())
@@ -80,12 +80,12 @@ namespace Player
                 _rigidbody.AddForce(Vector3.up * jumpMultiplier, ForceMode.Impulse);
             }
         }
-
+        
         private bool CanJump()
         {
             return _groundChecker.GetIsTouchingGround();
         }
-
+        
         /*
          * This single mechanic took me more time to get right than most of the other stuff combined
          * And it still doesn't work perfectly
@@ -97,5 +97,65 @@ namespace Player
             velocity = new Vector3(-((_rigidbody.position.x - (stoppingPoint + _mainCameraRigidbody.transform.position.x)) / returnToStoppingPointTime + playerSpeed), velocity.y, velocity.z);
             _rigidbody.velocity = velocity;
         }
+        
+        // private CharacterController _controller;
+        // private Vector3 _playerVelocity;
+        // private bool _groundedPlayer;
+        // [SerializeField] private float playerSpeed = 1.0f;
+        // [SerializeField] private float jumpHeight = 3.0f;
+        // [SerializeField] private float gravityValue = -9.81f;
+        //
+        // private GameMode _gameMode;
+        // private Rigidbody _mainCameraRigidbody;
+        //
+        // private void Start()
+        // {
+        //     _controller = gameObject.GetComponent<CharacterController>();
+        //     _gameMode = GetComponent<GameMode>();
+        //     _mainCameraRigidbody = GameObject.Find("Main_Camera").GetComponent<Rigidbody>();
+        // }
+        //
+        // private void HandleJump()
+        // {
+        //     // Changes the height position of the player..
+        //     if (Input.GetKeyDown(KeyCode.Space) && _groundedPlayer)
+        //     {
+        //         _playerVelocity.y += Mathf.Sqrt(jumpHeight * -3.0f * gravityValue);
+        //     }
+        //
+        //     _playerVelocity.y += gravityValue * Time.deltaTime;
+        //     _controller.Move(_playerVelocity * Time.deltaTime);
+        // }
+        //
+        // void Update()
+        // {
+        //     MovePlayer();
+        //     MoveCameraRight();
+        //     HandleJump();
+        // }
+        //
+        // private void MovePlayer()
+        // {
+        //     _groundedPlayer = _controller.isGrounded;
+        //     if (_groundedPlayer && _playerVelocity.y < 0)
+        //     {
+        //         _playerVelocity.y = 0f;
+        //     }
+        //
+        //     Vector3 move = new Vector3(_gameMode.GetCurrentWorldSpeed(), 0, 0);
+        //     _controller.Move(move * Time.deltaTime);
+        //
+        //     if (move != Vector3.zero)
+        //     {
+        //         gameObject.transform.forward = move;
+        //     }
+        //
+        //     
+        // }
+        //
+        // private void MoveCameraRight()
+        // {
+        //     _mainCameraRigidbody.velocity = new Vector3(_gameMode.GetCurrentWorldSpeed(), 0, 0);
+        // }
     }
 }
