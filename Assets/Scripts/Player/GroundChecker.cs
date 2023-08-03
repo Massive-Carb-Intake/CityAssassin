b3f8db717b2fe9cd76326c6203af42893bdd7c01
zenM49 <58347499+maarao@users.noreply.github.com>
2023-08-02 22:31:06 -0400

    Attempting Dying when hitting stuff

--- Assets/Scripts/Player/GroundChecker.cs
+++ Assets/Scripts/Player/GroundChecker.cs
@@ -5,16 +5,22 @@ namespace Player
     public class GroundChecker : MonoBehaviour
     {
         private bool _isTouchingGround = false;
-
         public bool GetIsTouchingGround()
         {
             return _isTouchingGround;
         }
 
+        private GameObject _collidedWith;
+        public GameObject GetCollidedWith()
+        {
+            return _collidedWith;
+        }
+
         // Without a rigidbody attached to Ground_Checker these functions will not work
-        private void OnCollisionEnter()
+        private void OnCollisionEnter(Collision other)
         {
             _isTouchingGround = true;
+            _collidedWith = other.gameObject;
         }
 
         private void OnCollisionExit()
